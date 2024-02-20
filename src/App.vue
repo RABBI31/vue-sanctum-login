4732edfc3b84c75d53cd0f886e9d719d902a3193
rabbi31 <18103380@iubat.edu>
2024-02-20 23:28:28 +0600

    check loging

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
- <div>
-  App
- </div>
+  <div>
+    <form @submit.prevent="login">
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <script setup>
+import axios from 'axios';
 
+const login = async () => {
+  // await axios.get('/sanctum/csrf-cookie');
+  await axios.post('/login', {
+    email: 'test@example.com',
+    password:'password'
+  })
+}
 </script>
-
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
-axios.defaults.withCredentials = true
-axios.defaults.baseURL = "http://localhost:8000"
+axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.withCredentials = true;
 
 createApp(App).mount('#app')
